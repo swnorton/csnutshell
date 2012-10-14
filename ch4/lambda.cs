4c7691ffacec430f0ff7f1ecca8d4d0988ddcba8
Steve Norton <stevenwnorton@yahoo.com>
2012-10-14 17:24:54 -0400

    added try.cs

--- ch4/lambda.cs
+++ ch4/lambda.cs
@@ -12,10 +12,13 @@ namespace Application
 		
 		static void Main()
 		{
-			int power = 2;
-			Func<int> func = new MakeFunc();
-			System.Console.WriteLine(func());
-			System.Console.WriteLine(func());
+			double power = 2;
+			Func<double,double> pwr = n => Math.Pow(n, power);
+			Console.WriteLine(pwr(5));
+			
+			Func<int> func = MakeFunc();
+			Console.WriteLine(func());
+			Console.WriteLine(func());
 		}
 	}
 }
